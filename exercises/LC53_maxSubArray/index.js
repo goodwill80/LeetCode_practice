026026a8bbeee9f4f43408c911059a601222fd18
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-10-30 21:41:30 +0800

    added max subarray

--- exercises/LC53_maxSubArray/index.js
+++ exercises/LC53_maxSubArray/index.js
@@ -1,3 +1,19 @@
-function maxSubArray(nums) {}
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+// [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// Starts from index 1
+// 1st loop - is 1 bigger or 1 + (-2) ? - we take the max of the 2
+// 2nd loop - is -3 bigger or -3 + (dpArr[2])
+function maxSubArray(nums) {
+  const dpArr = [nums[0]];
+
+  for (let i = 1; i < nums.length; i++) {
+    dpArr.push(Math.max(nums[i], nums[i] + dpArr[i - 1]));
+  }
+
+  return dpArr.sort((a, b) => a - b)[dpArr.length - 1];
+}
+
+maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
 
 module.exports = maxSubArray;
